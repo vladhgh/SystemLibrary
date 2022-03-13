9d1b6992246cd99bee5d7de03325e63566a3bdc2
vladhgh <vladhgh@gmail.com>
2022-03-13 23:09:54 +0300

    Added getObject function to test virtual polymorphism

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <memory>
+#include "src/Types/String.hpp"
+#include "src/Types/Double.hpp"
+
+
+auto getObject(const bool &returnString) -> std::unique_ptr<System::Object> {
+    if (returnString) {
+        return std::unique_ptr<System::String>{new System::String("Hola Hupa!")};
+    } else {
+        return std::unique_ptr<System::Double>{new System::Double(123.456)};
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto object = getObject(true);
+    auto stringObject = dynamic_cast<System::String*>(object.get());
+    std::cout << "\nReturned Type: " << object->GetType() << "\nValue: " << stringObject->ToString()  << std::endl;
+
+    auto object2 = getObject(false);
+    auto doubleObject = dynamic_cast<System::Double*>(object2.get());
+    std::cout << "\nReturned Type: " << object2->GetType() << "\nValue: " << doubleObject->ToString()  << std::endl;
     return 0;
 }
